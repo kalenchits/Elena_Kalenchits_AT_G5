ac3320442d6af577e2bb1f38d7dcfaf17433340d
kalenchits <elena.kalenchits@gmail.com>
2022-02-26 02:41:57 +0300

    Add Cycletask

---
+++ src/cycletask/PrintArray.java
@@ -0,0 +1,11 @@
+package cycletask;
+
+public class PrintArray {
+
+        public void printArray(int[] randomArray){
+
+            for (int i = 0; i < randomArray.length; i++){
+            System.out.print(randomArray[i] + " ");
+        }
+    }
+}
---
+++ src/cycletask/RandomArray.java
@@ -0,0 +1,18 @@
+package cycletask;
+
+import java.util.Random;
+
+public class RandomArray {
+
+    public static int[] fillRandomArray() {
+
+        int[] array = new int[7];
+        Random random = new Random();
+
+        for (int i = 0; i < 7; i++) {
+            array[i] = random.nextInt(20);
+            //System.out.print(array[i] + " ");
+        }
+        return array;
+    }
+}
