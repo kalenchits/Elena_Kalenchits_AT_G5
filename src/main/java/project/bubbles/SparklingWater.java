c5541bac2fdc696fb6f28c499a92816763a4e3a3
kalenchits <elena.kalenchits@gmail.com>
2022-03-11 02:52:28 +0300

    Add checkIsOpened

--- src/main/java/project/bubbles/SparklingWater.java
+++ src/main/java/project/bubbles/SparklingWater.java
@@ -1,12 +1,31 @@
 package main.java.project.bubbles;
 
-public class SparklingWater extends Water{
+import java.lang.Runnable;
+import java.lang.InterruptedException;
+
+public class SparklingWater extends Water implements Runnable{
 
     private Bubble bubble = new Bubble("CO2");
     private Bubble[] bubbles;
+    private boolean isOpened;
 
     public SparklingWater(String color, String transparency, String smell, int temperature) {
         super(color, transparency, smell, temperature);
+    }
+
+    private void checkIsOpened(){
+        Thread thread = run() -> {
+            while (!isOpened) {
+                System.out.println("Bottle is closed..");
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            degas();
+        };
+        thread.start();
     }
 
     public void pump(Bubble[] bubbles){
@@ -24,4 +43,8 @@ public class SparklingWater extends Water{
 
     }
 
+    @Override
+    public void run() {
+
+    }
 }
